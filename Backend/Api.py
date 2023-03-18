c2827157ce1a5c44b546c1a98d7724882f3fbc31
unknown <Eduardo - Meta@Eduardo-Meta.metapositiva.corp.br>
2023-03-18 13:24:43 -0300

    Update 1.1

--- Backend/Api.py
+++ Backend/Api.py
@@ -1,13 +1,13 @@
 import requests
 import json
-from integracao_Post import *
+from integracao import *
 
 def consulta():
     global dados_Logradouro, dados_Bairro, dados_Localidade
     try:
         #url para consulta do Cep
         url_Api = f'https://viacep.com.br/ws/{cep_usu()}/json/'
-        print(cep_usu)
+        print(cep_usu())
         requisicao = requests.get(url_Api)
         dados_Json = requisicao.content
         dados = json.loads(dados_Json)
@@ -15,9 +15,8 @@ def consulta():
         dados_Logradouro = dados['logradouro']
         dados_Bairro = dados["bairro"]
         dados_Localidade = dados['localidade']
-
+        return dados_Logradouro, dados_Bairro, dados_Localidade
     #Except para caso o usuario insira um cep invalido
     except:
-        print('Informe um CEP valido!')
-
-    return dados_Logradouro, dados_Bairro, dados_Localidade
+        print("Informe um CEP valido")
+        return "Informe um CEP valido"
--- Backend/integracao.py
+++ Backend/integracao.py
@@ -1,14 +1,32 @@
 import uvicorn as uvicorn
 from fastapi import FastAPI
 from Api import *
-from integracao_Post import *
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
+def cep_usu():
+    cep = '04434240'
+    return cep
+
+origins = [
+    "*"
+]
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+)
+@app.post('/postcep')
+async def post_cep():
+    return cep_usu()
 
 @app.get('/getdata')
 async def get_data():
     return consulta()
 
+
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0',port=7000)
+    uvicorn.run(app, host='0.0.0.0',port=7777)
